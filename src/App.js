54fdb054bf0c53fafee3e217a705f2f3dc6be3fc
blur <thetobiterra@gmail.com>
2023-03-08 14:54:27 +0100

    build

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 
+
+
 function App() {
   return (
     <div>
       <h1>Charlie's Place</h1>
       <div className="wrapper">
-        <div className='item'>item 1</div>
-        <div className='item'>item 2</div>
-        <div className='item'>item 3</div>
-        <div className='item'>item 4</div>
-        <div className='item'>item 5</div>
-        <div className='item'>item 6</div>
-        <div className='item'>item 7</div>
-        <div className='item'>item 8</div>
+        <div className='item item1'></div>
+        <div className='item item2'></div>
+        <div className='item item3'></div>
+        <div className='item item4'></div>
+        <div className='item item5'></div>
+        <div className='item item6'></div>
+        <div className='item item7'></div>
+        <div className='item item8'></div>
       </div>
       <h2>Top trending products</h2>
     </div>
